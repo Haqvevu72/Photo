d4c35f4cc7ae769efb58054c55208c072e8dcef5
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-06-28 05:32:35 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pictures> pictures = new List<Pictures>()
+        {
+            new Pictures("Pictures\\OIP.jpg"),
+            new Pictures("Pictures\\OIP (1).jpg"),
+            new Pictures("Pictures\\th.jpg")
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            PhotoShop.ItemsSource = pictures;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            StackPanel stackPanel = button.Parent as StackPanel;
+            TextBlock textBlock = stackPanel.Children[0] as TextBlock;
+            int index = int.Parse(textBlock.Text);
+            string sourcePath = pictures[index - 1].Source;
+            BitmapImage bitmapImage = new BitmapImage(new Uri(sourcePath, UriKind.RelativeOrAbsolute));
+            Canvas.Source = bitmapImage;
+
+        }
+
+        private void Width_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Canvas.Width = e.NewValue;
+        }
+
+        private void Height_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Canvas.Height = e.NewValue;
+        }
+    }
+}
---
+++ WpfApp1/Pictures.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Pictures
+    {
+        static int id = 0;
+        public int ID { get; set; }
+        public string Source { get; set; }
+
+        public Pictures(string source)
+        {
+            ID = ++id;
+            Source = source;
+        }
+
+
+    }
+}
